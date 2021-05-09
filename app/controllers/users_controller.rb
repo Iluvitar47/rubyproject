95d39b5790f29e167b1116d29ae8d67fb7a0c935
Iluvitar47 <theo.ramses.47@gmail.com>
2021-05-09 13:08:04 +0200

    devise4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     end
   end
   private def 
-  @user = User;find(params[:id])  
+  @user :: User;find(params[:id])  
   end 
   def user_params
   params.require(:user).permit(:email, :password)
