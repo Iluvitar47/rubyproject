70f2cc59b31d5871f29becafe71450caa5d5e01c
Iluvitar47 <theo.ramses.47@gmail.com>
2021-05-09 13:55:44 +0200

    2

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,6 @@ class Game < ApplicationRecord
     has_many :games_genres
     has_many :genres, through: :games_genres
     has_one_attached :thumbnail
-    belongs_to :user
 
     validates :thumbnail, attached: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  devise_for :users
-  devise_for :models
   root"games#index"
   resources :genres
   resources :games
