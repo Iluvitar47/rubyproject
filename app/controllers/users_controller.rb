3650e665d1da807b2c2e535013b6082c0db6ccc4
Iluvitar47 <theo.ramses.47@gmail.com>
2021-05-09 13:32:32 +0200

    devise14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,14 +19,13 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-  def 
-  @user.User;find(params[:id])  
-end-of-input
-end-of-input
+  #def 
+  #@user.User.find(params[:id])  
+  #end 
   def user_params
   params.require(:user).permit(:email, :password)
   end 
-end-of-input
+end
 
 #syntax error, unexpected '=', expecting '.' or :: (SyntaxError)
 #@user = User.find(params[:id]) 
